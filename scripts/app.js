54f03a8777822ccc3a9f821e6b3adb9277a2e3f5
Regin Guevarra <130509430+ReginGuevarra@users.noreply.github.com>
2023-05-26 13:50:12 -0700

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,17 +1,21 @@
 const searchParkType = document.getElementById('searchParkType');
 const searchLocation = document.getElementById('searchLocation');
-const parkContainer = document.getElementById("parkContainer");
+const parkContainer = document.getElementById('parkContainer');
 const clearButton = document.getElementById('clearButton');
 const mountainSelect = document.getElementById('mountainSelect');
+const mountainInfo = document.getElementById('mountainInfo');
+const mountainName = document.getElementById('mountainName');
+const mountainDescription = document.getElementById('mountainDescription');
+const mountainElevation = document.getElementById('mountainElevation');
 
 // Function to filter parks by state and park type
 const filterParks = () => {
   const selectedState = searchLocation.value;
   const selectedParkType = searchParkType.value;
 
-  parkContainer.innerHTML = "";
+  parkContainer.innerHTML = '';
 
-  if (selectedState === "" && selectedParkType === "") {
+  if (selectedState === '' && selectedParkType === '') {
     // Clear the park container when no filters are selected
     return;
   }
@@ -23,8 +27,8 @@ const filterParks = () => {
   });
 
   filteredParks.forEach(park => {
-    const cardElement = document.createElement("div");
-    cardElement.className = "col-md-4";
+    const cardElement = document.createElement('div');
+    cardElement.className = 'col-md-4';
 
     cardElement.innerHTML = `
       <div class="card mb-4">
@@ -37,7 +41,7 @@ const filterParks = () => {
             Zip Code: ${park.ZipCode}<br>
             Phone: ${park.Phone}
           </p>
-          ${park.Visit ? `<a href="${park.Visit}" class="card-link">Visit Website</a>` : ""}
+          ${park.Visit ? `<a href="${park.Visit}" class="card-link">Visit Website</a>` : ''}
         </div>
       </div>
     `;
@@ -47,33 +51,33 @@ const filterParks = () => {
 };
 
 // Add event listeners to the state and park type select elements
-searchLocation.addEventListener("change", filterParks);
-searchParkType.addEventListener("change", filterParks);
+searchLocation.addEventListener('change', filterParks);
+searchParkType.addEventListener('change', filterParks);
 
 // Initially load all parks
 filterParks();
 
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener('DOMContentLoaded', () => {
   // Populating the select elements
   parkTypesArray.forEach(parkType => {
-    const option = document.createElement("option");
+    const option = document.createElement('option');
     option.value = parkType;
     option.text = parkType;
     searchParkType.appendChild(option);
   });
 
   locationsArray.forEach(location => {
-    const option = document.createElement("option");
+    const option = document.createElement('option');
     option.value = location;
     option.text = location;
     searchLocation.appendChild(option);
   });
 
   // Populating the mountain select element
-  for (const mountain of mountainsArray) {
+  mountainsArray.forEach(mountain => {
     const option = new Option(mountain.name, mountain.name);
     mountainSelect.add(option);
-  }
+  });
 });
 
 // Function to clear selected filters and show all parks
@@ -85,3 +89,27 @@ const clearFilters = () => {
 
 // Add event listener to the clear button
 clearButton.addEventListener('click', clearFilters);
+
+// Function to update the displayed mountain information
+function updateMountainInfo() {
+  const selectedIndex = mountainSelect.selectedIndex;
+  if (selectedIndex !== -1) {
+    const selectedMountain = mountainsArray[selectedIndex];
+
+    mountainName.textContent = selectedMountain.name;
+    mountainDescription.textContent = selectedMountain.desc;
+    mountainElevation.textContent = `Elevation: ${selectedMountain.elevation} feet`;
+    // Update additional mountain information here
+  }
+}
+
+// Event listener for when a mountain is selected
+mountainSelect.addEventListener('change', () => {
+  const selectedIndex = mountainSelect.selectedIndex;
+  if (selectedIndex !== -1) {
+    updateMountainInfo();
+    mountainInfo.style.display = 'block';
+  } else {
+    mountainInfo.style.display = 'none';
+  }
+});
